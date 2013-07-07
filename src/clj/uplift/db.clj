d7c3980e7b0abc2a49134e09816c08ff71f8cebb
John Swanson <john.d.swanson@gmail.com>
2013-07-06 20:41:04 -0400

    datomic setup... incomplete

--- src/clj/uplift/db.clj
+++ src/clj/uplift/db.clj
@@ -1,11 +1,1 @@
-(ns uplift.db
-  (:use [uplift.config])
-  (:require [korma.core :refer [defentity database]]
-            [korma.db :refer [defdb mysql]]))
-
-(defmacro select-one [& stuff]
-  `(first (korma.core/select ~@stuff)))
-
-(defdb -mysql (mysql (config [:mysql-login])))
-
-(defentity session (database -mysql))
+(ns uplift.db)
