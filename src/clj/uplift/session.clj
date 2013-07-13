2666b7c33342289d7cdcee893942c581292a5576
John Swanson <john.d.swanson@gmail.com>
2013-07-12 17:02:28 -0700

    can retract session datum by setting it to nil

--- src/clj/uplift/session.clj
+++ src/clj/uplift/session.clj
@@ -29,10 +29,36 @@
 (defn read-data [conn key]
   (get-session conn key))
 
-(defn save-data [conn key data]
+(defn add-data [conn key data]
   (let [id-map (into {} (for [[k v] data] [k (:db/id v)]))]
     (d/transact conn [(conj {:db/id (d/tempid :workouts)
                              :session/session-id key} id-map)])))
+
+(defn data-to-retract-from-session
+  "Builds tx data to retract every attribute in seq data from our session"
+  [conn key data]
+  (when data
+    (seq (q '[:find ?op ?e ?a ?v
+              :in $ ?op [?a ...] ?k
+              :where [?e :session/session-id ?k]
+                     [?e ?a ?v]]
+            (db conn)
+            :db/retract
+            data
+            key))))
+
+(defn ret-data
+  "Builds transaction data to retract every specified session attribute, then
+  executes it"
+  [conn key data]
+  (when-let [tx (data-to-retract-from-session conn key data)]
+    (d/transact conn tx)))
+
+(defn save-data [conn key data]
+  (let [retracts (into {} (filter #(nil? (val %)) data))
+        additions (into {} (remove #(nil? (val %)) data))]
+    (do (ret-data conn key (keys retracts))
+        (add-data conn key additions))))
 
 (def cs (map char (concat (range 48 58) (range 66 92) (range 97 123))))
 (defn rand-char [] (nth cs (.nextInt (java.util.Random.) (count cs))))
@@ -46,6 +72,7 @@
 
 (defn delete-user-session [conn user]
   (let [sess-id (ffirst (q [:find '?sess-id
-                            :where ['?sess-id] ['?sess-id :session/user (:db/id user)]]
+                            :where ['?sess-id]
+                                   ['?sess-id :session/user (:db/id user)]]
                            (db conn)))]
     (d/transact conn [[:db.fn/retractEntity sess-id]])))
